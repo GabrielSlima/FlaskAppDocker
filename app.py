3e6043ba4574c2f6732dfafc4f80a5215463bdcf
gabrielslima <gabrielfatec61@gmail.com>
2019-03-24 08:14:32 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import jsonify
+import json
+import time
+
+app = Flask(__name__)
+
+simpleResponseData = '{"status": 200, "response": "Success!"}'
+
+
+@app.route('/app/api')
+def firebaseApi():
+    time.sleep(5)
+    return jsonify(json.loads(simpleResponseData))
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000)
